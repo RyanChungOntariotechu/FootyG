e27dc9291860dabd0341b0b2b58f28e25af9723f
RynoDyno <146674355+RyanChungOntariotechu@users.noreply.github.com>
2026-04-23 12:54:00 -0400

    added league display

---
+++ app.py
@@ -0,0 +1,26 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+headers = {"X-Auth-Token": "0006bd6221f04e118cfe53834f201eda"}
+BASE_URL = "https://api.football-data.org/v4/"
+
+def league_display():
+    url = f"{BASE_URL}competitions/"
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    competitions = data["competitions"]
+
+    leagues = [item for item in competitions if item["type"] == "LEAGUE"]
+    cups = [item for item in competitions if item["type"] != "LEAGUE"]
+
+    return leagues, cups
+
+@app.route("/")
+def home():
+    leagues, cups = league_display()
+    return render_template("index.html", leagues=leagues, cups=cups)
+
+if __name__ == "__main__":
+    app.run(debug=True)
